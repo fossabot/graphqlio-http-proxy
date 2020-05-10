83049195b6c2ab72a56f211c5f376a45c36da7a9
Andreas Pätzold <andreas.paetzold@msg-david.de>
2020-05-10 11:06:24 +0200

    Autowired was missing

--- src/main/java/com/graphqlio/server/http/proxy/service/WebSocketClientService.java
+++ src/main/java/com/graphqlio/server/http/proxy/service/WebSocketClientService.java
@@ -8,6 +8,7 @@ import com.graphqlio.wsf.domain.WsfFrame;
 import com.graphqlio.wsf.domain.WsfFrameType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.*;
 import org.springframework.web.socket.client.WebSocketClient;
@@ -42,6 +43,7 @@ public class WebSocketClientService {
     @SuppressWarnings("unchecked")
     private static BlockingMap<String, String> notificationMap = new BlockingMap();
 
+    @Autowired
     public WebSocketClientService(OutdatedPublisher outdatedPublisher) {
         this.outdatedPublisher = outdatedPublisher;
     }
