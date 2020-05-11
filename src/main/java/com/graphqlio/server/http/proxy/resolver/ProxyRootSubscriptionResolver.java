15e713e4cc8b437bedffa1c2a36116f268dcf573
edgar müller <edgar.mueller@msg.group>
2020-05-11 20:05:14 +0200

    GraphqlioHttpProxyService providing list of resolvers

--- src/main/java/com/graphqlio/server/http/proxy/resolver/ProxyRootSubscriptionResolver.java
+++ src/main/java/com/graphqlio/server/http/proxy/resolver/ProxyRootSubscriptionResolver.java
@@ -2,12 +2,13 @@ package com.graphqlio.server.http.proxy.resolver;
 
 import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLSubscriptionResolver;
 import com.graphqlio.server.http.proxy.reactive.ProxyPublisherWithSinkFactory;
 import com.graphqlio.server.http.proxy.reactive.ProxyPublisherWithSinkFactory.PublisherWithSink;
 
-
+@Component
 public class ProxyRootSubscriptionResolver implements GraphQLSubscriptionResolver {
 
 	@Autowired
